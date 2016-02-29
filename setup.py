39a6337fd0d02a2dba451dc795b6da5a815e4884
REDACTED <REDACTED_EMAIL>
2016-02-28 20:03:00 -0500

    Trying to get Travis to push to PyPI

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('LICENSE') as f:
 
 setup(
   name='isodatetimehandler',
-  version='1.0.1',
+  version='1.0.2',
   description='An alternate jsonpickle handler for datetime objects.',
   long_description=readme,
   author='Brian Hammond',
